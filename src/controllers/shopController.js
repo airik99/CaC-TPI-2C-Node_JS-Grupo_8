70852d3654e464a9bd8e615277cdb32abd887847
Isabel <veranoflia@yahoo.com.ar>
2023-12-07 18:37:20 -0300

    Get Shop/Carrito

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,13 +1,16 @@
-import productsData from '../model/products.json' assert { type: 'json' };;
+
+import productsData from '../model/products.json' assert { type: 'json' };
+import productsCarrito from '../model/carritoProducts.json' assert { type: 'json' };
 
 const shopControllers = {
     shop: (req, res)  => res.render('shop'),
-    //itemid:  (req, res)  => res.send(`Vista shop item id, siendo el id: ${req.params.id}`),
     itemid: (req, res)  => res.render('item', {
         product: productsData.find(product => product.product_id == req.params.id),
     }),
     itemidadd: (req, res)  => res.send('Vista añadir item al carrito'),
-    cartget: (req, res)  => res.send('Vista del carrito'),
+    cartget: (req, res)  => res.render('carrito', {
+        products: productsCarrito
+        }),
     cartpost: (req, res)  => res.send('Vista del carrito')
 }
 
